8802133c06808919c92902d96fafef8c96d07843
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2023-04-13 10:32:48 +0200

    Header Section and body

---
+++ uchindamiAstro/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className={"text-3xl bg-amber-600"}>Button</div>
+  )
+}
+
+export default Button
---
+++ uchindamiAstro/tailwind.config.cjs
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    "50": "#eff6ff",
+                    "100": "#dbeafe",
+                    "200": "#bfdbfe",
+                    "300": "#93c5fd",
+                    "400": "#60a5fa",
+                    "500": "#3b82f6",
+                    "600": "#2563eb",
+                    "700": "#1d4ed8",
+                    "800": "#1e40af",
+                    "900": "#1e3a8a"
+                },
+                "-primary-color": "rgb(132, 83, 57)",
+                "-secondary-color": "rgb(198, 139, 89)",
+                "-body-color": "rgb(224, 192, 151)",
+                "-container-color": "rgb(224, 192, 151)",
+                "-button-color-darkBg": "rgb(224, 192, 150)",
+                "-title-color": "rgb(224, 192, 151)",
+                "-text-color": "rgb(0, 0, 0)",
+                "-background-forInput": "rgb(214, 173, 138)",
+                "-hover-backGroundEffect": "rgb(78, 70, 70)",
+                "-input-color": "rgb(224, 192, 151)",
+            }
+        },
+        fontFamily: {
+            'body': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
+    },
+    plugins: [],
+}
